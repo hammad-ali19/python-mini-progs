9ec933a672485bac559092a8fc99c95a2e7ff9c0
Hammad Ali <chh68815@gmail.com>
2024-08-14 09:06:43 +0500

    first file

---
+++ 01_Problem.py
@@ -0,0 +1,20 @@
+# Write a Python function that takes a list of numbers as input and returns 
+# the sum of those numbers.
+
+def sum_of_numbers(numbers: list[int]) -> int:
+    if not numbers:
+        return -1
+    else:
+        sum: int = 0
+        for number in numbers:
+            sum = sum + number
+        return sum
+        
+        
+
+nums: list[int]= []
+Sum: int = sum_of_numbers(nums)
+if Sum == -1:
+    print("List was Empty! ")
+else:
+    print(f"Sum is {Sum}.")

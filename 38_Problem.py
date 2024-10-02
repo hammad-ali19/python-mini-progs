e8e88036b2daed43a5f1aa70ffba4a8418877f5b
Hammad Ali <chh68815@gmail.com>
2024-10-02 10:16:50 +0500

    Problem 17: 3Sum

---
+++ 38_Problem.py
@@ -0,0 +1,25 @@
+# Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, 
+# and j != k, and nums[i] + nums[j] + nums[k] == 0.
+
+def find_triplets (nums):
+    length = len(nums)
+    if length < 3:
+        return "Not a valid array"
+    nums.sort()
+    triplets = []
+
+    for i in range(length-2):
+        for j in range(i+1, length-1):
+            for k in range(j+1, length):
+                if nums[i] +  nums[j] + nums[k] == 0:
+                    lst = []
+                    lst.append(nums[i])
+                    lst.append(nums[j])
+                    lst.append(nums[k])
+                    if lst not in triplets:
+                        triplets.append(lst)
+
+    return triplets
+
+
+print(find_triplets([-1, 0, 1, 2, -1, -4]))

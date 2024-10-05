61badcd5a0b3954e1f42282e8365f04f11fb6130
Hammad Ali <chh68815@gmail.com>
2024-10-05 12:24:46 +0500

    Find 3SUM

--- 38_Problem.py
+++ 38_Problem.py
@@ -1,3 +1,4 @@
+# Problem 38: Find 3SUM
 # Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, 
 # and j != k, and nums[i] + nums[j] + nums[k] == 0.
 
